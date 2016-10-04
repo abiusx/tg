6771f0bc6b6c9c6ae676733081a265fe415f8c63
abiusx <git@abiusx.com>
2016-10-04 14:39:16 -0400

    support negative numbers for recent messages

--- telegrab-state.c
+++ telegrab-state.c
@@ -1,2 +1,13 @@
-printf ("%d %d %d %d\n",pts,qts,seq,date);
-// pts=1;
+// printf ("%d %d %d %d\n",pts,qts,seq,date);
+printf("You have already processed up to message %d. \
+To continue, press Enter, and to reprocess enter the message number you want to start at: ",pts);
+char msg[100];
+int result = scanf("%99[^\n]%*c", msg);
+if (result==1)
+{
+	int t=atoi(msg);
+	if (t<0)
+		pts+=t;
+	else
+		pts=t;
+}
